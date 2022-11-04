a5364f3455eac78d4e2f148c0f781d7bc8af2bc6
kH3nrique <92551677+kH3nrique@users.noreply.github.com>
2022-11-04 10:14:41 -0300

    first commit

---
+++ binario.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+main()
+{
+    //int ;
+    //char ;
+    //float ;
+}
+
---
+++ texto.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <conio.h>
+
+FILE *arq1, *arq2;
+main()
+{
+    char new_names[100];
+    if ((fopen("nomes.txt", "a+b") == NULL)){
+        printf("Problema na arbertura do arquivo\n");
+    }
+}
+
